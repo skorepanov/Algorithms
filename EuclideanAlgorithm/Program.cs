839e22e2c632a726d25c9cf0e4571f4ebd4e6b39
Sergey Korepanov <igzhestik@mail.ru>
2024-11-19 16:38:14 +0500

    Euclidean algorithm - Алгоритм Евклида

---
+++ EuclideanAlgorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿var firstNumber = 4851;
+var secondNumber = 3003;
+
+var result = gcd(firstNumber, secondNumber);
+
+Console.WriteLine($"НОД({firstNumber}, {secondNumber}) = {result}");
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        var remainder = a % b;
+        // НОД(A, B) = НОД(B, остаток)
+        a = b;
+        b = remainder;
+    }
+
+    return a;
+}
