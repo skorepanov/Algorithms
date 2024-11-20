2d1773305da943ff579280167e4697babce18b53
Sergey Korepanov <igzhestik@mail.ru>
2024-11-20 17:24:28 +0500

    Bubble sort - Сортировка пузырьком

---
+++ Sorting/BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿var sourceList = new List<int> { 20, 10, 15, 5, 1, 3, 2 };
+var sortedList = bubbleSort(sourceList);
+
+foreach (var item in sortedList)
+{
+    Console.WriteLine(item);
+}
+
+
+List<int> bubbleSort(List<int> list)
+{
+    for (var j = 1; j < list.Count; j++)
+    {
+        var isSorted = true;
+
+        for (var i = 0; i < list.Count - j; i++)
+        {
+            if (list[i] > list[i + 1])
+            {
+                (list[i], list[i + 1]) = (list[i + 1], list[i]);
+                isSorted = false;
+            }
+        }
+
+        if (isSorted)
+        {
+            break;
+        }
+    }
+
+    return list;
+}
