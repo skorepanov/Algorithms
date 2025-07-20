808f2075317fca6489f1eefb1669b000b6f8d835
Sergey Korepanov <igzhestik@mail.ru>
2025-07-20 21:27:22 +0500

    BinarySearch. Добавлены другие варианты алгоритма

--- Search/BinarySearch/Program.cs
+++ Search/BinarySearch/Program.cs
@@ -1,33 +1,130 @@
 ﻿var sourceList = new List<int> { 1, 2, 3, 5, 10, 15, 20 };
 var itemToSearch = 3;
 
-var index = binarySearch(sourceList, itemToSearch);
+var index1 = binarySearchTemplate1(sourceList, itemToSearch);
+var index2 = binarySearchTemplate2(sourceList, itemToSearch);
+var index3 = binarySearchTemplate3(sourceList, itemToSearch);
 
-Console.WriteLine($"Индекс элемента {itemToSearch}: {index ?? -1}");
+Console.WriteLine($"Вариант 1. Индекс элемента {itemToSearch}: {index1 ?? -1}");
+Console.WriteLine($"Вариант 2. Индекс элемента {itemToSearch}: {index2 ?? -1}");
+Console.WriteLine($"Вариант 3. Индекс элемента {itemToSearch}: {index3 ?? -1}");
+return;
 
-int? binarySearch(List<int> list, int item)
+// Вариант 1 - Наиболее базовый и элементарный
+int? binarySearchTemplate1(List<int> list, int target)
 {
-    var lowIndex = 0;
-    var highIndex = list.Count - 1;
-
-    while (lowIndex <= highIndex)
+    if (list.Count == 0)
     {
-        var middleIndex = lowIndex + (highIndex - lowIndex) / 2;
-        var guess = list[middleIndex];
+        return null;
+    }
 
-        if (guess == item)
+    var left = 0;
+    var right = list.Count - 1;
+
+    while (left <= right)
+    {
+        var middle = left + (right - left) / 2;
+
+        if (list[middle] == target)
         {
-            return middleIndex;
+            return middle;
         }
 
-        if (guess > item)
+        if (list[middle] > target)
         {
-            highIndex = middleIndex - 1;
+            right = middle - 1;
         }
         else
         {
-            lowIndex = middleIndex + 1;
+            left = middle + 1;
         }
+    }
+
+    // left == right + 1
+    // кандидатов больше нет
+    return null;
+}
+
+// Вариант 2 - Усовершенствованная версия варианта 1
+int? binarySearchTemplate2(List<int> list, int target)
+{
+    if (list.Count == 0)
+    {
+        return null;
+    }
+
+    var left = 0;
+    var right = list.Count - 1;
+
+    while (left < right)
+    {
+        var middle = left + (right - left) / 2;
+
+        if (list[middle] == target)
+        {
+            return middle;
+        }
+
+        if (list[middle] > target)
+        {
+            right = middle;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+    }
+
+    // left == right
+    // есть ещё 1 кандидат
+    if (list[left] == target)
+    {
+        return left;
+    }
+
+    return null;
+}
+
+// Вариант 3 - Альтернативный способ
+int? binarySearchTemplate3(List<int> list, int target)
+{
+    if (list.Count == 0)
+    {
+        return null;
+    }
+
+    var left = 0;
+    var right = list.Count - 1;
+
+    while (left + 1 < right)
+    {
+        var middle = left + (right - left) / 2;
+
+        if (list[middle] == target)
+        {
+            return middle;
+        }
+
+        if (list[middle] > target)
+        {
+            right = middle;
+        }
+        else
+        {
+            left = middle;
+        }
+    }
+
+    // left + 1 == right
+    // есть ещё 2 кандидата
+    if (list[left] == target)
+    {
+        return left;
+    }
+
+    if (list[right] == target)
+    {
+        return right;
     }
 
     return null;
